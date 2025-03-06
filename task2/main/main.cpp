2614a1f18bdc14a5902864d6f07d905cc9b17849
ISpeed2 <bro228nur@gmail.com>
2025-03-06 09:58:12 +0300

    Delete task2/main/main.cpp

--- task2/main/main.cpp
+++
@@ -1,8 +0,0 @@
-﻿int main() {
-    BackupJob job;
-    job.AddObject(BackupObject("example.txt"));
-    RestorePoint point = job.CreateRestorePoint();
-
-    ExceptionHandling::LogError("Тестовая ошибка");
-    return 0;
-}
